8e269705e84ff62e1d73734e79d2b15fdd00e9a9
axell-brendow <breno.axel@gmail.com>
2021-01-09 09:25:49 -0300

    Improve test message

--- src/main/main_test.go
+++ src/main/main_test.go
@@ -7,6 +7,6 @@ import (
 func TestSoma(t *testing.T) {
 	valor := soma(3, -4)
 	if valor != -1 {
-		t.Error("soma(3, -4) should be -1")
+		t.Errorf("soma(3, -4) should be -1 but got %d", valor)
 	}
 }
